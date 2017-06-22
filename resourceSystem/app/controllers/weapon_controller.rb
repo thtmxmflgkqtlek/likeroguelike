6fd2c83b2768efa61e50f3b386d54f43b1618866
minali372 <hirotori372@gmail.com>
2017-06-22 17:42:43 +0900

    処理を追記

--- resourceSystem/app/controllers/weapon_controller.rb
+++ resourceSystem/app/controllers/weapon_controller.rb
@@ -1,6 +1,13 @@
 class WeaponController < ApplicationController
   def index
-    @datas = Weapon.all
+    num = params(itemGetCount)
+    @datas = []
+    i = 0
+    for i < num do
+    x = Random.rand(0..255)
+    @datas.push(Weapon.find(x))
+    i = i + 1
+    end
   end
 
 end
